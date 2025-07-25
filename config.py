05e3d9b70a056445c47922768e3f984562d09c9e
VarunTejjj <varuntejkothapelli.co.in@gmail.com>
2025-07-25 09:19:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,21 +2,21 @@
 
 BOT_TOKEN = "8301262035:AAFFdlOpGUJ2tSv84VGOmpv-I5X-EpltkSc"
 
-# Telegram channel(s) where the bot listens (for product posts)
+# The Telegram channel where products are posted
 SOURCE_CHANNEL_ID = -1002750966898
 
-# Channels to force join before showing product info
+# Channels/groups users must join before accessing product info
 FORCE_JOIN_CHANNELS = [
-    -1002750966898,  # https://t.me/+yefc5k-8t1oxMDFl
-    -1002800054599   # https://t.me/anythinghere07
+    -1002750966898,  # your first channel
+    -1002800054599   # your second channel
 ]
 
-# Admin Telegram user ID
+# Admin Telegram user ID who approves products
 ADMIN_ID = 1831313735
 
 # Blogger API Credentials
 BLOGGER_OAUTH_TOKEN = "ya29.a0AS3H6NwJ2QUJLYDs-bFNqVvPcMOQZNaMirg4gZi1fSy3wPr-zIEKSk9ipYpO3fdP8Iik5LnUaJ43O02b9CwWDQnnMEQLv_zZv95iF9547NwdClXjruzrLLKFd-zL2I8m0MMPzgOE7Dbk9IYwMq-KI1kKWmsBDjntE6TOjsHaCgYKAc0SARcSFQHGX2Mifbyz9MGpjtpT2uxlh8qo-Q0175"
 BLOGGER_BLOG_ID = "8000505142904290143"
 
-# Your bot username without '@' (replace "yourbot" with actual username)
-BOT_USERNAME = "sovereigndealbot"
+# Your bot's username without '@'
+BOT_USERNAME = "yourbot"
