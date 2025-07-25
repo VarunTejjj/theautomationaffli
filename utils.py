6ce23bd254f9b6f384df8adf5528c69c4091edc6
VarunTejjj <varuntejkothapelli.co.in@gmail.com>
2025-07-25 08:23:36 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ import os
 PRODUCTS_FILE = "products.json"
 
 def load_products():
-    """Load all products from the local JSON database."""
+    """Load all products from JSON DB."""
     if not os.path.isfile(PRODUCTS_FILE):
         with open(PRODUCTS_FILE, "w") as f:
             json.dump({}, f)
@@ -14,12 +14,12 @@ def load_products():
         return json.load(f)
 
 def save_products(products):
-    """Save updated products to the local database."""
+    """Save products dict to file."""
     with open(PRODUCTS_FILE, "w") as f:
         json.dump(products, f, indent=2)
 
 def get_next_product_id(products):
-    """Returns the next available product ID as a string."""
+    """Generate the next product id as string."""
     if products:
         last_id = max(int(pid) for pid in products)
         return str(last_id + 1)
