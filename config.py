1202947b2829102e4d7c4d9c671c67d49cedea17
VarunTejjj <varuntejkothapelli.co.in@gmail.com>
2025-07-25 12:07:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,26 +1,16 @@
 # config.py
 
 BOT_TOKEN = "8301262035:AAFFdlOpGUJ2tSv84VGOmpv-I5X-EpltkSc"
-
-# Telegram channel where products are posted
 SOURCE_CHANNEL_ID = -1002750966898
 
-# Channels/groups users must join before accessing product info
-# Now stores both 'id' and 'invite_link' for private/public channels
 FORCE_JOIN_CHANNELS = [
     {"id": -1002750966898, "invite_link": "https://t.me/+yefc5k-8t1oxMDFl"},
     {"id": -1002800054599, "invite_link": "https://t.me/anythinghere07"}
 ]
 
-# Admin Telegram user ID
 ADMIN_ID = 1831313735
 
-# Blogger API Credentials - OAuth 2.0 client info and refresh token
-GOOGLE_CLIENT_ID = "768250259256-5a4uahtrvhnb349ikna1p289ekuq2tmd.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "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"
-GOOGLE_REFRESH_TOKEN = "ya29.a0AS3H6NwmKibYFAAOGN7pE8I_U3VZDoqs7uE--CpzAP5UiQZnSNYpk5bLAc1tNsTJ-NC-8PJFySvQrbJUz1oVnIdBTvn4ZjgZi6m6nFHRn3UrVRO1TPQoVpn1BY7iBDQKiw7FDfwU5s-dM1uV_YqaOsk5717sbEidIRcON-NgaCgYKAYISARcSFQHGX2MieXwbghfev9M9C1cWpaJDIw0175"
-# Blogger Blog ID
+# Static access token, must be fresh (manually updated when expired)
+BLOGGER_OAUTH_TOKEN = "your_new_valid_google_access_token"
 BLOGGER_BLOG_ID = "8000505142904290143"
-
-# Your bot's username without '@'
 BOT_USERNAME = "sovereigndealbot"
